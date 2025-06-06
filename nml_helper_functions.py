3b20fabd93589b5f4297423cff616a7e21b1f6d2
PatrickRiegert <83055508+PatrickRiegert@users.noreply.github.com>
2025-06-06 20:10:19 +0200

    Add files via upload

--- nml_helper_functions.py
+++ nml_helper_functions.py
@@ -1,43 +1,50 @@
 import tensorflow as tf
 
-def train_data(train_data_name, lablel_mode):
-  train_data_loaders = tf.keras.preprocessing.image_dataset_from_directory(directory=train_data_name, image_size=IMG_SIZE, label_mode=lablel_mode, batch_size=BATCH_SIZE)
-  return train_data_loaders
+# Define IMG_SIZE and BATCH_SIZE
+IMG_SIZE = (256, 256)  # Example size, adjust as needed
+BATCH_SIZE = 32  # Example batch size, adjust as needed
 
+def train_data(train_data_name, label_mode):
+    train_data_loaders = tf.keras.preprocessing.image_dataset_from_directory(
+        directory=train_data_name,
+        image_size=IMG_SIZE,
+        label_mode=label_mode,
+        batch_size=BATCH_SIZE)
+    return train_data_loaders
 
-def test_data(test_data_name, lablel_mode):
-  test_data_loaders = tf.keras.preprocessing.image_dataset_from_directory(directory=test_data_name, image_size=IMG_SIZE, label_mode=lablel_mode, batch_size=BATCH_SIZE)
-  return test_data_loaders
+def test_data(test_data_name, label_mode):
+    test_data_loaders = tf.keras.preprocessing.image_dataset_from_directory(
+        directory=test_data_name,
+        image_size=IMG_SIZE,
+        label_mode=label_mode,
+        batch_size=BATCH_SIZE)
+    return test_data_loaders
 
+def data_augmentation_creator_efficient_net(rflip, rrot, rzoom, rheight, rwidth, data_aug_name, input):
+    '''
+    Only for EfficientNet
+    '''
+    data_augmentation = tf.keras.Sequential([
+        tf.keras.layers.RandomFlip(rflip),
+        tf.keras.layers.RandomRotation(rrot),
+        tf.keras.layers.RandomZoom(rzoom),
+        tf.keras.layers.RandomHeight(rheight),
+        tf.keras.layers.RandomWidth(rwidth)],
+        name=data_aug_name)
+    augmented_tensor = data_augmentation(input)
+    return augmented_tensor
 
-def data_augmentation_creater_efficient_net(rflip, rrot, rzoom, rheight, rwidth, data_aug_name, input):
-  
-  '''
-  Only for EfficientNet
-  '''
-  data_augmentation = tf.keras.Sequential([
-      tf.keras.layers.RandomFlip(rflip),
-      tf.keras.layers.RandomRotation(rrot),
-      tf.keras.layers.RandomZoom(rzoom),
-      tf.keras.layers.RandomHeight(rheight),
-      tf.keras.layers.RandomWidth(rwidth)],
-      name=data_aug_name)
-  augmented_tensor = data_augmentation(input)
-  return augmented_tensor
-
-
-def data_augmentation_creater(rflip, rrot, rzoom, rheight, rwidth, rsale, data_aug_name, input):
-
-  '''
-  Not for EfficientNet
-  '''
-  data_augmentation = tf.keras.Sequential([
-      tf.keras.layers.RandomFlip(rflip),
-      tf.keras.layers.RandomRotation(rrot),
-      tf.keras.layers.RandomZoom(rzoom),
-      tf.keras.layers.RandomHeight(rheight),
-      tf.keras.layers.RandomWidth(rwidth),
-      tf.keras.layers.Rescaling(rsale)],
-      name=data_aug_name)
-  augmented_tensor = data_augmentation(input)
-  return augmented_tensor
+def data_augmentation_creator(rflip, rrot, rzoom, rheight, rwidth, rsale, data_aug_name, input):
+    '''
+    Not for EfficientNet
+    '''
+    data_augmentation = tf.keras.Sequential([
+        tf.keras.layers.RandomFlip(rflip),
+        tf.keras.layers.RandomRotation(rrot),
+        tf.keras.layers.RandomZoom(rzoom),
+        tf.keras.layers.RandomHeight(rheight),
+        tf.keras.layers.RandomWidth(rwidth),
+        tf.keras.layers.Rescaling(rsale)],
+        name=data_aug_name)
+    augmented_tensor = data_augmentation(input)
+    return augmented_tensor
